e15396e63a69b9b2ef43488fd9d071b80d150d0c
weblaunchdevrivoningo-dotcom <weblaunchdevrivoningo@gmail.com>
2025-12-15 15:15:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -72,4 +72,11 @@ function openVideo(source) {
 }
 
 // Load videos on page open
+
 document.addEventListener("DOMContentLoaded", loadVideos);
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.main-nav');
+
+hamburger.addEventListener('click', () => {
+  nav.classList.toggle('open');
+});
